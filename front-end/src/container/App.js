2c3a0ab1db45a9a817900cf8173832ba49b72e41
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-01-10 16:37:54 +0300

    create auction page is created

--- front-end/src/container/App.js
+++ front-end/src/container/App.js
@@ -5,10 +5,10 @@ import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
 import ProgressApi from '../shared/ProgressApi';
 import UserPage from '../pages/UserPage';
-
 import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import Navibar from '../components/NaviBar';
 import {connect} from 'react-redux'
+import CreateAuctionPage from '../pages/CreateAuctionPage';
 function App(props) {//Higher Order Component model should be use for ApiProgress but I cant find it how rfc HoC impl
   const login = <ProgressApi path="/api/auth/handle"><LoginPage/></ProgressApi>;
   const signup = <ProgressApi path="/users/createUser"><SignUpPage/></ProgressApi>
@@ -23,6 +23,7 @@ function App(props) {//Higher Order Component model should be use for ApiProgres
           {!loginState?<Route path="/login">{login}</Route>:""}
           {!loginState?<Route path="/signup">{signup}</Route>:""}
           <Route path="/users/:id" component={UserPage}/>
+          {loginState?<Route path="/createAuction">{CreateAuctionPage}</Route>:""}
           <Redirect to="/" />
         </Switch>
         
---
+++ front-end/src/services/auctionService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+class AuctionService {
+    
+}
+
+export default UserService;
